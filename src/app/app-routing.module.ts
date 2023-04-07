a5636b4e2db1c5f2e76d791a22f0506ba21bc909
Richard Thompson <rthompson624@gmail.com>
2023-04-07 14:07:40 -0400

    Add logged in guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,14 @@ import { PostsComponent } from './pages/posts/posts.component';
 import { UserViewComponent } from './pages/user-view/user-view.component';
 import { UserEditComponent } from './pages/user-edit/user-edit.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { isLoggedInGuard } from './guards/is-logged-in.guard';
 
 const ROUTES: Route[] = [
-  { path: 'users/:id/edit', component: UserEditComponent },
-  { path: 'users/:id', component: UserViewComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'posts', component: PostsComponent },
-  { path: 'albums', loadChildren: () => import('./albums/albums.module').then(m => m.AlbumsModule) },
+  { path: 'users/:id/edit', component: UserEditComponent, canActivate: [isLoggedInGuard] },
+  { path: 'users/:id', component: UserViewComponent, canActivate: [isLoggedInGuard] },
+  { path: 'users', component: UsersComponent, canActivate: [isLoggedInGuard] },
+  { path: 'posts', component: PostsComponent, canActivate: [isLoggedInGuard] },
+  { path: 'albums', loadChildren: () => import('./albums/albums.module').then(m => m.AlbumsModule), canActivate: [isLoggedInGuard] },
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
